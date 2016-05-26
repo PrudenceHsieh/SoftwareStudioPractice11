d8a14c363e5a6c6dd2d8e5b5cff26065dd6dda07
Prudence <hsyich53150@gmail.com>
2016-05-26 18:09:21 +0800

    lab11

---
+++ app/src/main/java/com/csclab/softwarestudiopractice11/MainActivity.java
@@ -0,0 +1,113 @@
+package com.csclab.softwarestudiopractice11;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import  android.content.Intent;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button send;
+    private EditText edt;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /** To Do:
+         * (1) Find the UI element on xml file
+         * **/
+
+        send = (Button) findViewById(R.id.sendName);
+        edt = (EditText) findViewById(R.id.nameKey);
+
+
+        send.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendName();
+            }
+        });
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void sendName() {
+
+        /** To Do:
+         * (1) Use intent and Bundle to pass string
+         * (2) Change activity to Activity2
+         * **/
+
+        String name = edt.getText().toString();
+
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this,Activity2.class);
+        Bundle bundle = new Bundle();
+
+        bundle.putString("name",name);
+        intent.putExtras(bundle);
+
+        startActivity(intent);
+
+
+
+
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.csclab.softwarestudiopractice11/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.csclab.softwarestudiopractice11/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
